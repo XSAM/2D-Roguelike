d440872b1665f1b6eba4871534fc8ee569e62cb3
XSAM <xbs2468@gmail.com>
2016-02-19 15:47:10 +0800

    Create Destructible walls

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite dmgSprite;
+    public int hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void DamageWall(int lose)
+    {
+        spriteRenderer.sprite = dmgSprite;
+        hp -= lose;
+        if (hp <= 0)
+            gameObject.SetActive(false);
+    }
+
+}
